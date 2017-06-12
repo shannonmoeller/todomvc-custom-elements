376f6e427b8209ddf5a788210389f8f805255902
Shannon Moeller <me@shannonmoeller.com>
2017-06-12 09:37:54 -0400

    Made server store stateful.

--- src/server/todo/todo-route.js
+++ src/server/todo/todo-route.js
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 import { createTodoStore, todoTemplates } from '../../shared/todo/todo-service';
 
-export function getTodo(request, response) {
-	const { state } = createTodoStore({
-		todoIndex: 2,
-		todos: [
-			{ id: 0, title: 'foo', completed: true },
-			{ id: 1, title: 'bar', completed: false },
-		],
-	});
+const store = createTodoStore({
+	todoIndex: 2,
+	todos: [
+		{ id: 0, title: 'foo', completed: true },
+		{ id: 1, title: 'bar', completed: false },
+	],
+});
 
+export function getTodo(request, response) {
 	response.send(
 		todoTemplates.renderIndex(
-			state
+			store.state
 		)
 	);
 }
